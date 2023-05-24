e5282723373724c621e8b02b0770fa3e6e776057
AdikaDS <adika.dwi.saputra@mail.ugm.ac.id>
2023-05-24 13:36:08 +0700

    initial commit

--- app/src/main/java/com/example/videoplayer/MainActivity.java
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -6,12 +6,15 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.webkit.URLUtil;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String LOCAL_VIDEO = "draw_video";
+    private static final String ONLINE_VIDEO = "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1";
+
     private VideoView mVideoView;
 
     @Override
@@ -19,17 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mVideoView = findViewById(R.id.video_view);
-    }
-
-    // Untuk mengeplay video dari local vutuh url
-    public Uri getMedia(String mediaName) {
-        return Uri.parse("android.resource://" + getPackageName() + "/raw/" + mediaName);
-    }
-
-    public void initializePlayer() {
-        // Memanggil video dari local
-        Uri videoUri = getMedia(LOCAL_VIDEO);
-        mVideoView.setVideoURI(videoUri);
 
         MediaController controller = new MediaController(this);
         controller.setMediaPlayer(mVideoView);
@@ -42,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
                 mVideoView.start();
             }
         });
+    }
+
+    public Uri getMedia(String mediaName) {
+        if (URLUtil.isValidUrl(mediaName)) {
+            return Uri.parse(mediaName);
+        } else {
+            // Untuk mengeplay video dari local butuh url
+            return Uri.parse("android.resource://" + getPackageName() + "/raw/" + mediaName);
+
+        }
+    }
+
+    public void initializePlayer() {
+//        // Memanggil video dari local
+//        Uri videoUri = getMedia(LOCAL_VIDEO);
+//        mVideoView.setVideoURI(videoUri);
+        // Memanggil video dari local
+        Uri videoUri = getMedia(ONLINE_VIDEO);
+        mVideoView.setVideoURI(videoUri);
+        mVideoView.start();
     }
 
     public void relesaePlayer() {
